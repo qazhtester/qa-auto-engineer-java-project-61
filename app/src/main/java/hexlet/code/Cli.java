b06770bc382b5012addd754c108143f0df780421
marchenko zhanna <zmarchenko@alfabank.ru>
2025-12-14 15:57:16 +0300

    Интерактивное взаимодействие с пользователем

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public final class Cli {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private Cli() {
+    }
+
+    static void greetUser() {
+        System.out.println("May I have your name?");
+        String name = SCANNER.nextLine();
+        System.out.printf("Hello, %s!%n", name);
+    }
+}
