df79c40e3653c889512f689d5246c928538ab93b
marchenko zhanna <zmarchenko@alfabank.ru>
2025-12-17 15:38:47 +0300

    Добавлен javadoc  в RandomUtils.java

--- app/src/main/java/hexlet/code/RandomUtils.java
+++ app/src/main/java/hexlet/code/RandomUtils.java
@@ -9,17 +9,22 @@ public final class RandomUtils {
     }
 
     /**
-     * Генерирует случайное целое число
-     * в диапазоне от  {@code min} до {@code max} <b>включительно</b>
-     **/
+     * Генерирует случайное целое число в диапазоне от {@code min} до {@code max} включительно.
+     *
+     * @param min минимальное значение (включительно)
+     * @param max максимальное значение (включительно)
+     * @return случайное целое число в заданном диапазоне
+     */
     public static int getRandomInt(int min, int max) {
         return RANDOM.nextInt(min, max + 1);
     }
 
     /**
-     * Генерирует случайное целое число
-     * в диапазоне от 0 до {@code max} <b>включительно</b>
-     **/
+     * Генерирует случайное целое число в диапазоне от 0 до {@code max} включительно.
+     *
+     * @param max максимальное значение (включительно)
+     * @return случайное целое число в заданном диапазоне
+     */
     public static int getRandomInt(int max) {
         return getRandomInt(0, max);
     }
